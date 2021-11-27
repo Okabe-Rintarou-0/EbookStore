7ae8cd4cb3ad3671ff1509ecc10f57c65aa4da0f
okabe <923048992@qq.com>
2021-11-27 16:33:58 +0800

    deploy eureka

--- catstore-backend/src/main/java/com/catstore/CatStoreApplication.java
+++ catstore-backend/src/main/java/com/catstore/CatStoreApplication.java
@@ -3,6 +3,7 @@ package com.catstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -12,6 +13,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 @EnableAsync
 @EnableScheduling //开启定时任务
 @SpringBootApplication
+@EnableEurekaClient
 @ServletComponentScan
 public class CatStoreApplication {
     public static void main(String[] args) {
